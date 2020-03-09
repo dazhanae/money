f58ad07369854c830f81f7f59bf38130d0fb4dcb
Dazhanae <dazhanaep@gmail.com>
2020-03-09 16:40:18 -0400

    yeet

---
+++ src/main/java/MAIN.java
@@ -0,0 +1,11 @@
+
+
+public class MAIN {
+    public static void main(String[] args) {
+        int x = 10;
+        double p =23.0;
+        float y = 2.3f;
+        float answer = x * y;
+        System.out.println(answer);
+    }
+}
---
+++ src/test/java/javaday1.java
@@ -0,0 +1,5 @@
+public class javaday1 {
+    public static void main(String[] args) {
+        System.out.println("rehab is for quitters");
+    }
+}
